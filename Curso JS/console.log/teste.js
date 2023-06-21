31e2d20ddb8863bae9cc817269129ef5e60b60f5
JhonathanHU3 <137320981+JhonathanHU3@users.noreply.github.com>
2023-06-21 19:19:37 -0300

    21/06/23 19:19

--- Curso JS/console.log/teste.js
+++ Curso JS/console.log/teste.js
@@ -1,9 +1,6 @@
-console.log("hello word")
-console.log("hello word")
-console.log("hello word")
-console.log("hello word")
-console.log("hello word")
-console.log("hello word")
-console.log("hello word")
-console.log("hello word")
-console.log("hello word")
+console.log("hello world") //String = texto
+console.log('Hello "world"')
+console.log(`Hello world`)
+
+console.log (234, 19.5) //Number
+console.log ("i am a", 7) //string+number
