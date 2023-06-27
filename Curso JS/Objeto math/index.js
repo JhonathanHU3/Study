076eb26aec382a30d7c60ef2c312a9baef0e01c8
JhonathanHU3 <137320981+JhonathanHU3@users.noreply.github.com>
2023-06-26 23:48:40 -0300

    26/06/23

---
+++ Curso JS/Objeto math/index.js
@@ -0,0 +1,9 @@
+let num1 = 9.666;
+
+// let num2 = Math.floor(num1); arredonda um número para baixo
+// let num2 = Math.ceil(num1); arredonda um número para cima
+// let num2 = Math.round(num1); arredonda um número conforme regra matemática
+// let num2 = Math.max(1,2,3,5,300,10,123,222,4454,2123122,4544) pega o maior número de alguma sequência
+// let num2 = Math.min(1,2,3,5,300,10,123,222,4454,2123122,4544) pega o menor número de alguma sequência
+const randon = Math.random() * (5) + 5;
+console.log(randon);
