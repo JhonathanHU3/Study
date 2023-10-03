5822bb1ef94eea0f5c8b46dd00e0a875b1db692e
Jhonathan Pimenta <137320981+JhonathanHU3@users.noreply.github.com>
2023-10-03 19:54:13 +0000

    ex js 03/10 1654

--- Exercicios ES6/Arrays/reduce.js
+++ Exercicios ES6/Arrays/reduce.js
@@ -1,9 +1,16 @@
 // Equilibrio de parênteses
-const arrEx1 = ['((()))','(()))','()(()','(((())((']
+const arrEx1 = ['((()))','(()))','()(()','(((())(('];
 
-function validaParentreses(arrEx1){
-    arrEx1.reduce((arrayboolean ,parentese) => {
-        parentese 
-        arrayboolean.push(parentese)
-    })
-}
+let arrayboolean = arrEx1.reduce((arrayboolean ,parentese) => {
+        let soma = 0;
+        if(parentese === '(') {soma++};
+        if(parentese === ')') {soma--};
+        console.log(parentese)
+        console.log(soma)
+        if(soma !== 0) {parentese = false} else {parentese = true};
+        console.log(parentese)
+        arrayboolean.push(parentese);
+        return arrayboolean;
+    },[])
+
+console.log(arrayboolean);
