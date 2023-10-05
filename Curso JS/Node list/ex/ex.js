4f038e022d62921ac084e73673dcf1084fe7e38c
JhonathanHU3 <137320981+JhonathanHU3@users.noreply.github.com>
2023-10-05 19:21:54 -0300

    updt

--- Curso JS/Node list/ex/ex.js
+++ Curso JS/Node list/ex/ex.js
@@ -1,8 +1,8 @@
 let paragrafos = document.querySelector(".paragrafos");
 let listaP = paragrafos.querySelectorAll("p");
-let backgroundColor = (getComputedStyle(document.querySelector('body'))).backgroundColor;
+let bg = (getComputedStyle(document.querySelector('body'))).backgroundColor;
 
 for(let p in listaP){
-    listaP[p].style.backgroundColor = backgroundColor;
+    listaP[p].style.backgroundColor = bg;
     listaP[p].style.color = 'white';
 }
