a50f5a1164b82d46829ef7571ac666ee46f10854
Jhonathan Pimenta <jhonathanscp@gmail.com>
2023-12-18 01:25:25 -0300

    181223 0125

--- Curso JS/Try e catch/script.js
+++ Curso JS/Try e catch/script.js
@@ -7,7 +7,7 @@
 
 function soma(num1, num2) {
     if (typeof num1 !== "number" || typeof num2 !== "number") {
-        throw new ReferenceError("num1 e num2 não são números");
+        throw new ReferenceError("num1 e/ou num2 não são números");
     }
 
     return num1 + num2;
@@ -17,6 +17,6 @@ try{
     console.log(soma(4, 8));
     console.log(soma("4", 8));
 } catch(error) {
-    // console.log(error);
+    console.log(error);
     console.log("Alguma coisa mais amigável para o usuário")
 }
---
+++ Curso JS/setInterval & setTimeout/ex/ex.js
@@ -0,0 +1,44 @@
+function timer() {
+    const initButton = document.querySelector('#init')
+    const pauseButton = document.querySelector('#pause')
+    const resetButton = document.querySelector('#reset')
+    const clock = document.querySelector('#clock')
+
+    initButton.addEventListener('click', initTimer);
+    pauseButton.addEventListener('click', pauseTimer);
+    resetButton.addEventListener('click', resetTimer);
+
+    let timer;
+    let timerValue = 0;
+
+    function initTimer() {
+        clearInterval(timer);
+        timer = setInterval(() => {
+            clock.innerHTML = millisecondsToTime(timerValue++);
+        }, 1000);
+        clock.style.color = 'black';
+    }
+
+    function pauseTimer() {
+        clearInterval(timer);
+        clock.style.color = 'red';
+    }
+
+    function resetTimer() {
+        timerValue = 0;
+        clock.innerHTML = millisecondsToTime(timerValue);
+        clearInterval(timer);
+        clock.style.color = 'black';
+    }
+
+    function millisecondsToTime(seconds) {
+        const date = new Date(seconds * 1000);
+
+
+        return date.toLocaleTimeString('pt-BR', {
+            hour12: false,
+            timeZone: 'UTC'
+        });
+    }
+}
+timer();
